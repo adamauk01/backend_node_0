dc35577867558a63aeb4d7604011dfded7922c56
vanh601 <vanh18601k@gmail.com>
2024-05-05 22:19:56 +0700

    use connection pool

--- src/server.js
+++ src/server.js
@@ -5,10 +5,19 @@ const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web.js');
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME || 'localhost';
-
+const connection = require('./config/database.js')
 configViewEngine(app);
 app.use('/', webRoutes);
 
+//test connection
+
+
+// connection.query(`SELECT * FROM Users`,
+//   function (err, results, fields) {
+//     console.log('result = ', results);
+//     console.log('field = ', fields);
+//   }
+// )
 
 
 app.listen(port, hostname, () => {
