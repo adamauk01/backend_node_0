482366a450f83f20ca6a7d15df198fae299242ad
vanh601 <vanh18601k@gmail.com>
2024-05-05 18:41:27 +0700

    use static file and view engine

--- src/server.js
+++ src/server.js
@@ -5,10 +5,13 @@ const path = require('path');
 
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME || 'localhost';
+
+
 //config template engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+//config static file
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('sample.ejs');
 })
